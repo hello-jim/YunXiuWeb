3ebfe6fac823a602fae2147c598a515b10ebb5c5
hello-jim <842736598@qq.com>
2016-10-17 09:12:29 +0800

    ...

--- YunXiuWeb/Presentation/BrnMall.Web/controllers/CommonProblemController.cs
+++ YunXiuWeb/Presentation/BrnMall.Web/controllers/CommonProblemController.cs
@@ -51,6 +51,178 @@ namespace BrnMall.Web.Controllers
         {
             return View();
         }
+        public ActionResult CLCX()
+        {
+            return View();
+        }
+        public ActionResult CLYZ()
+        {
+            return View();
+        }
+        public ActionResult DDCF()
+        {
+            return View();
+        }
+        public ActionResult DDCLGZ()
+        {
+            return View();
+        }
+        public ActionResult DDCX()
+        {
+            return View();
+        }
+        public ActionResult DDQD()
+        {
+            return View();
+        }
+        public ActionResult DDQX()
+        {
+            return View();
+        }
+        public ActionResult DDTJ()
+        {
+            return View();
+        }
+        public ActionResult DDXG()
+        {
+            return View();
+        }
+        public ActionResult DDYC()
+        {
+            return View();
+        }
+        public ActionResult FHGZ()
+        {
+            return View();
+        }
+        public ActionResult FHJZ()
+        {
+            return View();
+        }
+        public ActionResult FHQS()
+        {
+            return View();
+        }
+        public ActionResult FHYZ()
+        {
+            return View();
+        }
+        public ActionResult FPJZ()
+        {
+            return View();
+        }
+        public ActionResult FPYZ()
+        {
+            return View();
+        }
+        public ActionResult GWLC()
+        {
+            return View();
+        }
+        public ActionResult PHOME()
+        {
+            return View();
+        }
+        public ActionResult JFZP()
+        {
+            return View();
+        }
+        public ActionResult JYTK()
+        {
+            return View();
+        }
+        public ActionResult JZNR()
+        {
+            return View();
+        }
+        public ActionResult JZZR()
+        {
+            return View();
+        }
+        public ActionResult MSJZ()
+        {
+            return View();
+        }
+        public ActionResult MSYZ()
+        {
+            return View();
+        }
+        public ActionResult QSJZ()
+        {
+            return View();
+        }
+        public ActionResult QSYZ()
+        {
+            return View();
+        }
+        public ActionResult SDPJ()
+        {
+            return View();
+        }
 
+        public ActionResult SHTK()
+        {
+            return View();
+        }
+        public ActionResult SHWT()
+        {
+            return View();
+        }
+          public ActionResult SPZX()
+        {
+            return View();
+        }
+          public ActionResult THFX()
+          {
+              return View();
+          }
+          public ActionResult THJZ()
+          {
+              return View();
+          }
+          public ActionResult THYF()
+          {
+              return View();
+          }
+          public ActionResult THYZ()
+          {
+              return View();
+          }
+          public ActionResult WLPS()
+          {
+              return View();
+          }
+          public ActionResult WQTJ()
+          {
+              return View();
+          }
+          public ActionResult WTCL()
+          {
+              return View();
+          }
+          public ActionResult YFJZ()
+          {
+              return View();
+          }
+          public ActionResult YFYZ()
+          {
+              return View();
+          }
+          public ActionResult ZLJC()
+          {
+              return View();
+          }
+          public ActionResult ZLJZ()
+          {
+              return View();
+          }
+          public ActionResult ZLYZ()
+          {
+              return View();
+          }
+          public ActionResult ZZ()
+          {
+              return View();
+          }
     }
 }
